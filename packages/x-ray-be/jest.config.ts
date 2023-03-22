c99b202014cb8a8c5ecb674069b4fcd473e74962
doooox <dtopic12@gmail.com>
2023-03-22 20:29:29 +0100

    initial commit

---
+++ packages/x-ray-be/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'x-ray-be',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/packages/x-ray-be',
+};
